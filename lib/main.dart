25b635b107e544afb7a839d916d342d5765cb3ea
muina <jordanmuina@gmail.com>
2025-07-10 15:47:59 -0700

    ajoute l'initialisation de firebase dans le projet

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:eommerce_app/presentation/splash/bloc/splash_cubit.dart';
 import 'package:eommerce_app/presentation/splash/pages/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'core/configs/theme/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async{
+  /// Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
